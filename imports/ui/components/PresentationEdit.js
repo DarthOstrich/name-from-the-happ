5743ebace7cfe31b13786c544c500e3857489672
Vonetta <vonettastevenson@gmail.com>
2017-05-24 21:19:31 -0700

    presentation edit

--- imports/ui/components/PresentationEdit.js
+++ imports/ui/components/PresentationEdit.js
@@ -13,7 +13,8 @@ class PresentationEdit extends Component {
 
   render() {
     return (
-    <div>
+		<div className="boxed-view">
+		  <div className="boxed-view__box">
       <h1>Presentation Edit</h1>
       <h2>Edit Presentation</h2>
 
@@ -27,41 +28,48 @@ class PresentationEdit extends Component {
           {/* <div class="slider"></div> */}
         </label>
 
+        <br/>
+
 {/* Toggle switch to see if it is completed */}
         <label htmlFor="">Status (Completed/Not Present)
           <input type="checkbox"
             ref={(completed) => { this.completed = completed; }}/>
             {/* <div class="slider"></div> */}
         </label>
-
+  <br/>
 {/* section for inputting the presentation title */}
         <label htmlFor="">Presentation Tile
           <input type="text" placeholder="Input Presentation Tile"
             ref={(presTitle) => { this.presTitle = presTitle; }}/>
         </label>
-
+  <br/>
 {/* Toggle switch to see if it is a group project */}
         <label htmlFor="">Group presentation?
           <input type="checkbox" ref={(groupPres) => { this.groupPres = groupPres; }}/>
           {/* <div class="slider"></div> */}
         </label>
-
+  <br/>
 {/* section for group Presenters */}
-        <label htmlFor="">Presenter(s)
+        <label htmlFor="">Presenter(s):
           <input type="text" placeholder="Name 1" ref={(presId1) => { this.presId1 = presId1; }}/>
+          <button type="submit">X</button>
           <input type="text" placeholder="Name 2" ref={(presId2) => { this.presId2 = presId2; }}/>
+          <button type="submit">X</button>
           <input type="text" placeholder="Name 3" ref={(presId3) => { this.presId3 = presId3; }}/>
+          <button type="submit">X</button>
         </label>
-
+  <br/>
 {/* section for additional notes */}
         <label htmlFor="">Notes
           <input type="textarea" ref={(addNotes) => { this.addNotes = addNotes; }}/>
         </label>
-
-{/* Submit button */}
-        <button type="submit">Submit</button>
+  <br/>
+{/* Submit buttons */}
+        <button className="" type="submit">Update</button>
+        <button type="submit">Delete</button>
       </form>
     </div>
+  </div>
 // end of return
     );
 // end of render
