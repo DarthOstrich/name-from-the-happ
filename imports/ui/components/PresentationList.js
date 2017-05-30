8be15447a54baa028a0bdf017fb6a9457399b034
Lauren Tapley <lntapley@gmail.com>
2017-05-30 12:41:28 -0700

    in class testing out presentationlist.js, added dynamic code

--- imports/ui/components/PresentationList.js
+++ imports/ui/components/PresentationList.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-// components
+// collections
+import { Sections } from './../../api/sections';
 
-const presentationList = () => (
-  <div className="container">
-    <h1>GWDA407</h1>
-    <ul>
-      <li>Presentation Title</li>
-      <li>Presentation Title</li>
-    </ul>
-    <button><Link to="/presentationadd">Add Presentation</Link></button>
-  </div>
-  // END .container
-);
+class presentationList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      links: [],
+    };
+  }
+  componentDidMount() {
+    console.log('componentDidMount SetionList');
+
+    this.linksTracker.autorun(() => {
+      const links = Sections.find().fetch();
+      this.setState({
+        links,
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.renderSectionId}</h1>
+        <ul>
+          <li>Presentation Title</li>
+          <li>Presentation Title</li>
+        </ul>
+        <button><Link to="/presentationadd">Add Presentation</Link></button>
+      </div>
+   // END .container
+  );
+  }
+}
 
 export default presentationList;
